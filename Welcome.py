a8dd5fd10130be93453c3fcbb7fe4f24d562a7e5
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 09:42:42 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -7,7 +7,7 @@ for i in os.listdir():
   if i.endswith('csv'):
     file_name_list.append(i)
 
-st.write(file_name_list)
+
 
 st.write('Hello World')
 
@@ -16,3 +16,5 @@ st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
+
+st.selectbox('select location', file_name_list)
