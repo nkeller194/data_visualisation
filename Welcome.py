fd5d5a3c755628a3c83ecaebd20faf39e3116fc7
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 09:41:01 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pandas as pd
+import os
+
+file_name_list = []
+for i in os.listdir():
+  if i.endswith('csv'):
+    file_name_list.append(i)
+
+st.write(file_name_list)
 
 st.write('Hello World')
 
@@ -7,4 +15,4 @@ df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
-st.selectbox('select element', el_list)
+x_axis = st.selectbox('select element', el_list)
