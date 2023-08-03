e6c44ad0134a5681869998bf009dd062e2071841
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 12:03:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -14,7 +14,7 @@ for i in os.listdir():
 st.write('Hello World')
 
 
-el_list = df.columns.tolist()[27:100]
+el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list[0])
