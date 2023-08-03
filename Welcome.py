13b4db9c75ac705077652a6f3d27c222467d683b
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 10:05:38 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+import bokeh
 
 file_name_list = []
 for i in os.listdir():
@@ -21,7 +23,3 @@ x_axis = st.selectbox('select element', el_list)
 st.multiselect('select location', file_name_list, file_name_list[0])
 
 
-el_x = 'Mg'
-el_y = 'Si'
-plt.scatter(df[el_x], df[el_y])
-plt.show()
