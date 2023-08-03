820d0536510953ea0e1997e2b819558ec02a42ce
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 10:39:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -15,7 +15,7 @@ st.write('Hello World')
 
 for i in file_name_list:
   df = pd.read_csv(str(i))
-  st.dataframe(i)
+  st.dataframe(df)
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
