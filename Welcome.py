b1ab4dccfcdc58f65517a8cd0c6279cb8ff6cc2d
nkeller194 <141241955+nkeller194@users.noreply.github.com>
2023-08-03 12:05:38 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -13,6 +13,9 @@ for i in os.listdir():
 
 st.write('Hello World')
 
+df = pd.read_csv('Bastar Craton.csv')
+st.dataframe(df)
+
 
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('select element', el_list)
